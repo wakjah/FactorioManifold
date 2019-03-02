12f9f9481c9387da789b301480a71a8a363d6c11
Will Keith <wakjah@gmail.com>
2019-03-02 00:11:49 +0000

    Updates to work with 0.17

--- src/prototypes/technology.lua
+++ src/prototypes/technology.lua
@@ -24,9 +24,9 @@ data:extend({
     {
       count = 200,
       ingredients = {
-        {"science-pack-1", 1},
-        {"science-pack-2", 1},
-        {"science-pack-3", 1},
+        {"automation-science-pack", 1},
+        {"logistic-science-pack", 1},
+        {"chemical-science-pack", 1},
       },
       time = 30
     },
