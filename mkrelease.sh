344b916f97f5eb769a27d40386789e70d3065ab6
Will Keith <wakjah@gmail.com>
2019-03-10 15:22:30 +0000

    Tweak release script so that it archives changes in working dir

--- mkrelease.sh
+++ mkrelease.sh
@@ -10,4 +10,9 @@ mod_dir_name="${mod_name}_${mod_version}"
 mod_zip_name="${mod_dir_name}.zip"
 
 # Create the zip
-git archive -o "releases/$mod_zip_name" --prefix "$mod_dir_name/" HEAD:src
+# Create a stash so that we get local changes in the working dir
+stash_name=$(git stash create)
+git stash store $stash_name # Store it so we can later call drop on it
+git archive -o "releases/$mod_zip_name" --prefix "$mod_dir_name/" $stash_name:src
+# Cleanup; really we should put this in an exit hook or something in case the above errors
+git stash drop stash@{0}
